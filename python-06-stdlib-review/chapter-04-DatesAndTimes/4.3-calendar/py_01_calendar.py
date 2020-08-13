013f9291a195b921f5f2b0a37fa600274d2619de
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-08-13 19:58:39 +0800

    Add files via upload

--- python-06-stdlib-review/chapter-04-DatesAndTimes/4.3-calendar/py_01_calendar.py
+++ python-06-stdlib-review/chapter-04-DatesAndTimes/4.3-calendar/py_01_calendar.py
@@ -1,9 +1,12 @@
 """
 ! what?
-
+the calendar module focuses **mostly** on printing full calendars in various formats.
+it also provides functinons useful for working with dates in other ways, 
+such as calculating dates for a recurring events
 
 ! why?
-
+why not? check some YT video to 
+see why we don't create or implement time by ourselves.
 
 ! how?
 
