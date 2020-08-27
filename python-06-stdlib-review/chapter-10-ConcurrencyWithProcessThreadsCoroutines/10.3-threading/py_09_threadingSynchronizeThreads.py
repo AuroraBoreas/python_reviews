731bafa527f60874ba7e6bc133ef9cce4745793d
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-08-28 00:02:43 +0800

    Add files via upload

--- python-06-stdlib-review/chapter-10-ConcurrencyWithProcessThreadsCoroutines/10.3-threading/py_09_threadingSynchronizeThreads.py
+++ python-06-stdlib-review/chapter-10-ConcurrencyWithProcessThreadsCoroutines/10.3-threading/py_09_threadingSynchronizeThreads.py
@@ -66,6 +66,41 @@ def threading_barrier():
 
     pass
 
+@addBreaker
+def threading_barrier_abort():
+    def worker(barrier):
+        print(threading.current_thread().name, 'waiting for barrier with {} others'.format(barrier.n_waiting))
+        try:
+            worker_id = barrier.wait()
+        except threading.BrokenBarrierError:
+            print(threading.current_thread().name, 'aborting')
+        else:
+            print(threading.current_thread().name, 'after barrier', worker_id)
+    
+    NUM_THREADS = 3
+
+    barrier = threading.Barrier(NUM_THREADS + 1)
+    threads = [
+        threading.Thread(
+        name='worker-%s' % i,
+        target=worker,
+        args=(barrier,),
+        )
+        for i in range(NUM_THREADS)
+    ]
+
+    for t in threads:
+        print(t.name, 'starting')
+        t.start()
+        time.sleep(0.1)
+        barrier.abort()
+        
+    for t in threads:
+        t.join()
+    pass
+
+
+
 if __name__ == "__main__":
     """
     In addition to using Events, another way of synchronizing threads is by using a Condition
@@ -89,3 +124,5 @@ if __name__ == "__main__":
     ### block until all of the participating “parties” have reached that point.
     ### With this approach, threads can start up separately and then pause until they are all ready to proceed.
     threading_barrier()
+    ### barrier abort()
+    threading_barrier_abort()
