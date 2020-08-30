442c1bc7399ee567e729ad36bc071319dcf84de4
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-08-30 22:01:46 +0800

    Add files via upload

--- python-06-stdlib-review/chapter-10-ConcurrencyWithProcessThreadsCoroutines/10.5-asyncio/py_00_asyncio.py
+++ python-06-stdlib-review/chapter-10-ConcurrencyWithProcessThreadsCoroutines/10.5-asyncio/py_00_asyncio.py
@@ -43,6 +43,43 @@ illustration as follows.
 
 asyncio
 
+mechanism
+The mechanism for yielding control back to the event loop depends on Python’s coroutines, 
+special functions that give up control to the caller without losing their state.
 
+Coroutines are quite similar to generator functions; in fact, generators can be used to implement
+coroutines in versions of Python earlier than 3.5 without native support for coroutine objects.
+
+生产计划员
+`asyncio` also provides a class-based abstraction layer for protocols and transports for
+writing code using callbacks instead of writing coroutines directly.
+
+生产计划
+A `future` is a data structure representing the result of work that has not been completed
+yet. The event loop can watch for a Future object to be set to done, thereby allowing one
+part of an application to wait for another part to finish some work. Besides futures, asyncio
+includes other concurrency primitives such as locks and semaphores.
+
+A `Task` is a subclass of Future that knows how to wrap and manage the execution of a
+coroutine. An event loop schedules tasks to run when the resources they need are available,
+and to produce a result that can be consumed by other coroutines.
+
+
+asyncio: Asynchronous I/O, Event Loop, and Concurrency Tools
+|-- Asynchronous Concurrency Concepts
+|-- Cooperative Multitasking with Coroutines
+|-- Scheduling Calls to regular functions
+|-- Producing Results Asynchronously
+|-- Executing Tasks Concurrently
+|-- Composing Coroutines with Control Structures
+|-- Synchronization Primitives
+|-- Asynchronous I/O with Protocol Class Abstractions
+|-- Asynchronous I/O using coroutines and streams
+|-- Using SSL
+|-- Interacting with Domain Name Services
+|-- Wokring with Subprocesses
+|-- Receiving Unix Signals
+|-- Combining Coroutines with Threads and Processes
+|-- Debugging with asyncio
 
 """
