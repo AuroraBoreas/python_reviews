476849550021cfad70ab0d765d776f07a1040ddb
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-09-03 00:01:22 +0800

    Add files via upload

--- python-06-stdlib-review/chapter-10-ConcurrencyWithProcessThreadsCoroutines/10.6-concurrent/py_08_concurrent.futuresProcessPools.py
+++ python-06-stdlib-review/chapter-10-ConcurrencyWithProcessThreadsCoroutines/10.6-concurrent/py_08_concurrent.futuresProcessPools.py
@@ -7,11 +7,25 @@ def futures_process_pool_map():
     def task(n):
         return (n, os.getpid())
 
-    ex = futures.ProcessPoolExecutor(max_workers=2)
-    results = ex.map(task, range(5, 0, -1))
+    with futures.ProcessPoolExecutor(max_workers=2) as ex:
+        results = ex.map(task, range(5, 0, -1))
     """
-    If something happens to one of the worker processes that causes it to exit unexpectedly,
-    the ProcessPoolExecutor is considered “broken” and will no longer schedule tasks.
+    ! AttributeError: Can't pickle local object 'futures_process_pool_map.<locals>.task'
+    
+    The problem is that multiprocessing must pickle things to sling them among processes, 
+    and bound methods are not picklable. 
+    The workaround (whether you consider it "easy" or not;-) is to 
+    add the infrastructure to your program to allow such methods to be pickled, 
+    registering it with the copy_reg standard library method.    
+
+    In theory, python can't pickle functions. (for details, see Can't pickle Function)
+    In practice, python pickles a function's name and module so that passing a function will work. 
+
+    Pool needs to pickle (serialize) everything it sends to its worker-processes (IPC). 
+    Pickling actually only saves the name of a function and unpickling requires re-importing the function by name. 
+    For that to work, the function needs to be defined at the top-level, 
+    nested functions won't be importable by the child and already trying to pickle them raises an exception (more).
+
     """
     for n, pid in results:
         logging.debug(f'ran task {n} in process {pid}')
