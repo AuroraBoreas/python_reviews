c0cbc94bf0c8d10f55d48778aa74bb5779a76311
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-09-09 10:49:18 +0800

    Add files via upload

---
+++ python-06-stdlib-review/chapter-14-ApplicationBuildingBlocks/14.1-argparse/py_00_argparse.py
@@ -0,0 +1,20 @@
+"""
+! what?
+`argparse` includes tools for building command-line argument
+and option processors.
+
+! why?
+why not.
+
+! how?
+
+argparse: Command-Line Option and Argument Parsing
+|-- Setting Up a Parser
+|-- Defining arguments
+|-- Parsing a Command Line
+|-- Simple Examples
+|-- Help Output
+|-- Parser Organization
+|-- Advanced Argument Processing
+
+"""
