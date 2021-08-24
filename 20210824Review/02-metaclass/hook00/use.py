4413291dc1fe5010fe6f4acbe9cd34b483ffcccc
AuroraBoreas <AuroraBoreas@users.noreply.github.com>
2021-08-24 22:00:57 +0800

    20210824Review

---
+++ 20210824Review/02-metaclass/hook00/use.py
@@ -0,0 +1,13 @@
+"Python is a protocol orientated lang; every top-level function has a corresponding dunder method implemented;" 
+
+from lib import Base
+
+assert hasattr(Base, 'foo'), AttributeError(f'foo not found in {Base}')
+
+class Derived(Base):
+    def bar(self):
+        return self.foo()
+
+class A(Base):
+    def a(self):
+        return 'a'
---
+++ 20210824Review/04-generator/gen03.py
@@ -0,0 +1,13 @@
+"Python is a protocol orientated lang; every top-level function has a corresponding dunder method implemented;" 
+import time
+from typing import List, TypeVar
+T = TypeVar('T', int, float, complex)
+
+def compute(n:int)->int:
+    for i in range(n):
+        yield i
+        time.sleep(.5)
+
+if __name__ == '__main__':
+    for i in compute(10):
+        print(i)
