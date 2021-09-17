0b0655562595d6a8d4f58ccc9248f3d99df9eea1
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-09-17 08:38:22 +0800

    Add files via upload

---
+++ 20210917Review/04-generator/gen03.py
@@ -0,0 +1,17 @@
+"Python is a protocol orientated lang; every top-level function has a dunder method implemented;" 
+
+import time
+from typing import List, TypeVar
+T = TypeVar('T', int, float, complex)
+
+import contextlib
+
+@contextlib.contextmanager
+def compute(n:int)->int:
+    for i in range(n):
+        yield i
+        time.sleep(.5)
+
+if __name__ == '__main__':
+    for i in compute(10):
+        print(i)
