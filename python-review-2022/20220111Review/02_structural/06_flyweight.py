243a0866f3176a8bb5a0d5cfd0a388ef02287f6b
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2022-01-11 11:17:19 +0800

    Add files via upload

---
+++ python-review-2022/20220111Review/02_structural/06_flyweight.py
@@ -0,0 +1,39 @@
+# 
+import json
+from typing import Dict, List
+
+class Flyweight:
+    _shared_state:str = None
+
+    def __init__(self, shared_state:str) -> None:
+        self._shared_state = shared_state
+
+    def operation(self, unique_state:str)->None:
+        s = json.dumps(self._shared_state)
+        u = json.dumps(unique_state)
+        print(f'{self.__class__} : shared -> {s}; unique -> {u}')
+
+class FlyweightFactory:
+    _flyweights:Dict[str, Flyweight] = {}
+
+    def __init__(self, init_flyweights:Dict) -> None:
+        for state in init_flyweights:
+            self._flyweights[self.get_state(state)] = Flyweight(state)
+
+    def get_state(self, shared_state:List)->str:
+        return '_'.join(sorted(shared_state))
+
+    def get_flyweight(self, shared_state:List)->Flyweight:
+        key = self.get_state(shared_state)
+
+        if not self._flyweights.get(key):
+            print(f'{self.__class__} : {shared_state} not found; creating a new one..')
+            self._flyweights[key] = Flyweight(shared_state)
+        else:
+            print(f'{self.__class__} : reusing the exiting one..')
+
+        return self._flyweights[key]
+
+    def list_flyweights(self)->None:
+        print('\n'.join(map(str, self._flyweights.keys())))
+    
---
+++ python-review-2022/20220111Review/03_behavioral/10_visitor.py
@@ -0,0 +1,26 @@
+# 
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import overload
+
+
+class IGraphic(ABC):
+    @abstractmethod
+    def accept(self, v:Visitor)->None:
+        raise NotImplementedError()
+
+class Dot(IGraphic):
+    def accept(self, v: Visitor) -> None:
+        v.visit(self)
+
+class Shape(IGraphic):
+    def accept(self, v: Visitor) -> None:
+        v.visit(self)
+
+class Visitor:
+    @overload
+    def visit(self, d:Dot)->None:
+        print(f'{self.__class__} : visited {d.__class__}')
+
+    def visit(self, s:Shape)->None:
+        print(f'{self.__class__} : visited {s.__class__}')
